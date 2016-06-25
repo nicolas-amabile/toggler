987e8e3ea5c1b4a772ce19ea5d761d0c00c06f60
nicoamabile <amabile.nicolas@gmail.com>
2016-06-24 21:33:12 -0300

    Fix export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-module.exports = {
-  Tabs: require('./tabs.js'),
-  Pane: require('./pane.js')
-};
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+function _interopRequire(obj) { return obj && obj.__esModule ? obj['default'] : obj; }
+
+var _Tabs = require('./tabs.js');
+exports.Tabs = _interopRequire(_Tabs);
+
+var _Pane = require('./pane.js');
+exports.Pane = _interopRequire(_Pane);
