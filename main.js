7166cbbe3372906f073144b6ee19448c0d5c988a
nicoamabile <amabile.nicolas@gmail.com>
2016-06-24 21:43:31 -0300

    Add build step

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+function _interopRequire(obj) { return obj && obj.__esModule ? obj['default'] : obj; }
+
+var _Tabs = require('./tabs.js');
+exports.Tabs = _interopRequire(_Tabs);
+
+var _Pane = require('./pane.js');
+exports.Pane = _interopRequire(_Pane);
